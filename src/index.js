54e7d0efe1350ada8518d0335409730e7efa5523
Dominique FABRE <dfabre@clever-age.com>
2023-01-11 15:47:32 +0100

    Components 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 
+import App from './App'
+
 const root = ReactDOM.createRoot(document.getElementById('app-root'))
 
 root.render(React.createElement(
-  'div',
+  App,
   {id: 'app'},
   'My nice App'
 ))
